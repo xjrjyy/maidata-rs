fe2ad2c91189bff2bec77caf2dfed996c514b9dd
jrjyy <1479412322@qq.com>
2023-12-24 12:15:06 +0800

    增加解析目录内谱面文件功能

---
+++ src/bin/maidata-parse/main.rs
@@ -0,0 +1,59 @@
+use walkdir::WalkDir;
+
+fn main() {
+    let dir = std::env::args().nth(1).expect("usage: $0 <path/to/charts>");
+
+    for entry in WalkDir::new(dir)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !e.file_type().is_dir())
+    {
+        if entry.file_name() == "maidata.txt" {
+            println!("{:?}", entry.path());
+            parse_maidata(entry.path());
+        }
+    }
+}
+
+fn parse_maidata<P: AsRef<std::path::Path>>(path: P) {
+    let content = read_file(path);
+    let maidata = maidata::container::lex_maidata(&content);
+
+    println!("title = {}", maidata.title());
+    println!("artist = {}", maidata.artist());
+
+    for diff in maidata.iter_difficulties() {
+        use std::borrow::Cow;
+
+        println!();
+        println!("difficulty {:?}", diff.difficulty());
+        println!(
+            "  level {}",
+            diff.level()
+                .map_or(Cow::Borrowed("<not set>"), |x| Cow::Owned(format!("{}", x)))
+        );
+        println!(
+            "  offset {}",
+            diff.offset()
+                .map_or(Cow::Borrowed("<not set>"), |x| Cow::Owned(format!("{}", x)))
+        );
+        println!("  designer {}", diff.designer().unwrap_or("<not set>"));
+        println!(
+            "  static message {}",
+            diff.single_message().unwrap_or("<not set>")
+        );
+
+        let mut mcx = maidata::materialize::MaterializationContext::with_offset(0.0);
+        let notes = mcx.materialize_insns(diff.iter_insns());
+        println!("  <{} notes materialized>", notes.len());
+
+        for insn in diff.iter_insns() {
+            println!("{:?}", insn);
+        }
+    }
+}
+
+fn read_file<P: AsRef<std::path::Path>>(path: P) -> String {
+    let content = std::fs::read(path.as_ref()).expect("file reading failed");
+    String::from_utf8(content).expect("decoding file content as utf-8 failed")
+}
